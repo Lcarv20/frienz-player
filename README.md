466e4e68b50b7c9b342d6fc8db532c00db2bbe0f
Luís Carvalho <42584819+Lcarv20@users.noreply.github.com>
2022-07-26 21:53:29 +0200

    Creating README

---
+++ README.md
@@ -0,0 +1,6 @@
+## SETUP ##
+
+- Create account on Firebase and initialize a project
+- Copy project credentials to the .env_template and remove the "_template" from the extension
+- Go to the Authentication tab on firebase.com and activate Sign in with email and password
+- run ```npm i && npm run tauri dev``` to start the dev mode
--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -3,14 +3,18 @@ import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+const envV = import.meta.env;
+
+console.log("key", import.meta.env.FB_API_KEY);
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
-	apiKey: "AIzaSyBVxEftRRxjpbHSfSOUJ6pxIRWNFrDkUEc",
-	authDomain: "tauri-player.firebaseapp.com",
-	projectId: "tauri-player",
-	storageBucket: "tauri-player.appspot.com",
-	messagingSenderId: "853673949754",
-	appId: "1:853673949754:web:f72b5ccf18feafdaa4d303",
+	apiKey: envV.VITE_FB_API_KEY,
+	authDomain: envV.VITE_FB_AUTH_DOMAIN,
+	projectId: envV.VITE_FB_PROJECT_ID,
+	storageBucket: envV.VITE_FB_STORAGE_BUCKET,
+	messagingSenderId: envV.VITE_FB_MESSAGING_SENDER_ID,
+	appId: envV.VITE_FB_APP_ID,
 };
 
 // Initialize Firebase
